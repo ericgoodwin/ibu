24b5dce902666d9183d2f7b9c7c5e4b5670b566e
Eric Goodwin <eric@goodwin.co>
2020-03-17 21:06:44 -0700

    Update all Ibu method to return {:ok, any()} or {:error, any()}

--- lib/ibu.ex
+++ lib/ibu.ex
@@ -1,41 +1,50 @@
 defmodule Ibu do
-  use Tesla
+  use Tesla, only: [:get]
 
   plug(Tesla.Middleware.BaseUrl, "https://biathlonresults.com/modules/sportapi/api/")
   plug(Tesla.Middleware.Headers, [{"Accept", "application/json"}])
   plug(Tesla.Middleware.JSON)
+  plug(Tesla.Middleware.Logger)
 
   alias Ibu.{Athlete, Event, Result, Race}
 
-  @spec search_athletes(binary, binary | nil) :: {:ok, [Athlete.t()]}
+  @spec search_athletes(binary, binary | nil) :: {:ok, [Athlete.t()]} | {:error, any()}
   def search_athletes(last_name, first_name \\ nil) do
-    {:ok, response} = get("Athletes?FamilyName=#{last_name}&GivenName=#{first_name}")
+    case get("Athletes?FamilyName=#{last_name}&GivenName=#{first_name}") do
+      {:error, reason} ->
+        {:error, reason}
 
-    result =
-      response
-      |> Map.get(:body)
-      |> Map.get("Athletes")
-      |> Enum.filter(fn athlete ->
-        Map.get(athlete, "Functions") == "Athlete"
-      end)
-      |> Enum.map(&Athlete.build_from_api/1)
+      {:ok, response} ->
+        result =
+          response
+          |> Map.get(:body)
+          |> Map.get("Athletes")
+          |> Enum.filter(fn athlete ->
+             Enum.member?(["Athlete" ,"Not active"], Map.get(athlete, "Functions"))
+          end)
+          |> Enum.map(&Athlete.build_from_api/1)
 
-    {:ok, result}
+        {:ok, result}
+    end
   end
 
-  @spec get_events(integer) :: {:ok, [Event.t()]}
+  @spec get_events(integer) :: {:ok, [Event.t()]} | {:error, any()}
   def get_events(season_id \\ 1920) do
-    {:ok, response} = get("Events?SeasonId=#{season_id}")
+    case get("Events?SeasonId=#{season_id}") do
+      {:error, reason} ->
+        {:error, reason}
 
-    result =
-      response
-      |> Map.get(:body)
-      |> Enum.filter(fn event ->
-        Map.get(event, "Level") == 1
-      end)
-      |> Enum.map(&Event.build_from_api/1)
+      {:ok, response} ->
+        result =
+          response
+          |> Map.get(:body)
+          |> Enum.filter(fn event ->
+            Map.get(event, "Level") == 1
+          end)
+          |> Enum.map(&Event.build_from_api/1)
 
-    {:ok, result}
+        {:ok, result}
+    end
   end
 
   @doc """
@@ -45,16 +54,20 @@ defmodule Ibu do
     iex> Ibu.get_event_races("BT1920SWRLCP08")
     {:ok, [%Ibu.Race{...}]}
   """
-  @spec get_event_races(binary) :: {:ok, [Race.t()]}
+  @spec get_event_races(binary) :: {:ok, [Race.t()]} | {:error, any()}
   def get_event_races(event_id) do
-    {:ok, response} = get("Competitions?EventId=#{event_id}")
+    case get("Competitions?EventId=#{event_id}") do
+      {:error, reason} ->
+        {:error, reason}
 
-    result =
-      response
-      |> Map.get(:body)
-      |> Enum.map(fn data -> Race.build_from_api(Map.put(data, "EventId", event_id)) end)
+      {:ok, response} ->
+        result =
+          response
+          |> Map.get(:body)
+          |> Enum.map(fn data -> Race.build_from_api(Map.put(data, "EventId", event_id)) end)
 
-    {:ok, result}
+        {:ok, result}
+    end
   end
 
   @doc """
@@ -64,63 +77,70 @@ defmodule Ibu do
     iex> Ibu.get_race_details("BT1920SWRLCP08MXRL")
     {:ok, %Ibu.Race{...}, %Ibu.Event{...}, []}
   """
-  @spec get_race_details(binary) :: {:ok, Race.t(), Event.t(), list}
+  @spec get_race_details(binary) :: {:ok, Race.t(), Event.t(), list} | {:error, any()}
   def get_race_details(race_id) do
-    {:ok, response} = get("Results?RaceId=#{race_id}")
+    case get("Results?RaceId=#{race_id}") do
+      {:error, reason} ->
+        {:error, reason}
 
-    body = Map.get(response, :body)
+      {:ok, response} ->
+        body = Map.get(response, :body)
 
-    event =
-      body
-      |> Map.get("SportEvt")
-      |> Event.build_from_api()
-
-    race = Map.get(body, "Competition")
-    race = Map.put(race, "EventId", event.ibu_id)
-    race = Race.build_from_api(race)
-
-    results =
-      case(Race.complete?(race)) do
-        true ->
+        event =
           body
-          |> Map.get("Results")
-          |> Enum.map(fn result -> Result.build_from_api(result, race_id) end)
+          |> Map.get("SportEvt")
+          |> Event.build_from_api()
 
-        false ->
-          {:error, :race_not_final}
-      end
+        race = Map.get(body, "Competition")
+        race = Map.put(race, "EventId", event.ibu_id)
+        race = Race.build_from_api(race)
 
-    {:ok, race, event, results}
+        results =
+          case(Race.complete?(race)) do
+            true ->
+              body
+              |> Map.get("Results")
+              |> Enum.map(fn result -> Result.build_from_api(result, race_id) end)
+
+            false ->
+              {:error, :race_not_final}
+          end
+
+        {:ok, race, event, results}
+    end
   end
 
   @spec get_race_results(binary) :: {:ok, [Result.t()]} | {:error, binary}
   def get_race_results(race_id) do
     case get("Results?RaceId=#{race_id}") do
+      {:error, reason} ->
+        {:error, reason}
+
       {:ok, %{body: %{"IsResult" => true, "Results" => results}}} ->
-        result = Enum.map(results, fn data -> Result.build_from_api(data, race_id) end)
+        result =
+          results
+          |> Enum.map(fn data -> Result.build_from_api(data, race_id) end)
+
         {:ok, result}
 
       {:ok, %{body: %{"IsResult" => false}}} ->
         {:error, :race_not_final}
-
-      _ ->
-        {:error, :unknown_error}
     end
   end
 
-  @spec get_athlete_details(binary) :: {:ok, Athlete.t()}
+  @spec get_athlete_details(binary) :: {:ok, Athlete.t()} | {:error, any()}
   def get_athlete_details(ibu_id) do
     case get("CISBios?IBUId=#{ibu_id}") do
+      {:error, reason} ->
+        {:error, reason}
+
       {:ok, response} ->
         result =
-        response
-        |> Map.get(:body)
-        |> Athlete.build_from_api()
+          response
+          |> Map.get(:body)
+          |> Athlete.build_from_api()
+
         {:ok, result}
-      {:error, :invalid_uri} -> {:error, :invalid_uri}
-
     end
-
-
   end
 end
--- test/ibu_test.exs
+++ test/ibu_test.exs
@@ -1,8 +1,67 @@
 defmodule IbuTest do
   use ExUnit.Case
-  doctest Ibu
+  # doctest Ibu
 
-  test "greets the world" do
-    assert Ibu.hello() == :world
+  setup do
+    Tesla.Mock.mock(fn
+      %{
+        method: :get,
+        url:
+          "https://biathlonresults.com/modules/sportapi/api/Athletes?FamilyName=fourcade&GivenName="
+      } ->
+        %Tesla.Env{
+          status: 200,
+          body: %{
+            "Athletes" => [
+              %{
+                "Age" => 31,
+                "Birthdate" => "1988-09-14T00:00:00",
+                "FamilyName" => "FOURCADE",
+                "Functions" => "Athlete",
+                "GenderId" => "M",
+                "GivenName" => "Martin",
+                "IBUId" => "BTFRA11409198801",
+                "NAT" => "FRA",
+                "NF" => "FRA",
+                "otherFamilyNames" => nil,
+                "otherGivenNames" => nil
+              },
+              %{
+                "Age" => 35,
+                "Birthdate" => "1984-04-25T00:00:00",
+                "FamilyName" => "FOURCADE",
+                "Functions" => "Official",
+                "GenderId" => "M",
+                "GivenName" => "Simon",
+                "IBUId" => "BTFRA12504198401",
+                "NAT" => "FRA",
+                "NF" => "FRA",
+                "otherFamilyNames" => nil,
+                "otherGivenNames" => nil
+              }
+            ]
+          }
+        }
+    end)
+
+    :ok
+  end
+
+  test "search_athletes" do
+    response = Ibu.search_athletes("fourcade")
+
+    assert {:ok,
+            [
+              %Ibu.Athlete{
+                birth_date: ~D[1988-09-14],
+                country_code: "FRA",
+                family_name: "FOURCADE",
+                flag_uri: nil,
+                gender: "M",
+                given_name: "Martin",
+                ibu_id: "BTFRA11409198801",
+                photo_uri: nil
+              }
+            ]} = response
   end
 end
