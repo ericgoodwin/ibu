ce649f25f8abb8807aa3bf8ab3d6cfb0d14a7a0c
Eric Goodwin <eric@goodwin.co>
2020-03-17 21:05:53 -0700

    Rename Ibu.Helper to DateHelper and add tests

---
+++ lib/date_helper.ex
@@ -0,0 +1,40 @@
+defmodule Ibu.DateHelper do
+  @spec to_birth_date(nil) :: nil
+  def to_birth_date(nil), do: nil
+
+  @spec to_birth_date(binary) :: Date.t()
+  def to_birth_date(str) when is_binary(str) do
+    {:ok, birth_date, _offset} =
+      str
+      |> fix_incorrect_datetime_format
+      |> transform_date_format_to_datetime
+      |> DateTime.from_iso8601
+
+    birth_date |> DateTime.to_date()
+  end
+
+  @spec to_date_time(nil) :: nil
+  def to_date_time(nil), do: nil
+
+  @spec to_date_time(binary) :: DateTime.t()
+  def to_date_time(str) do
+    {:ok, parsed_date, _offset} = DateTime.from_iso8601(str)
+    parsed_date
+  end
+
+  @spec fix_incorrect_datetime_format(binary) :: binary
+  defp fix_incorrect_datetime_format(str) do
+    case Regex.match?(~r/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$/, str) do
+      true -> str <> "Z"
+      false -> str
+    end
+  end
+
+  @spec transform_date_format_to_datetime(binary) :: binary
+  defp transform_date_format_to_datetime(str) do
+    case Regex.match?(~r/^\d{4}-\d{2}-\d{2}$/, str) do
+      true -> str <> "T00:00:00Z"
+      false -> str
+    end
+  end
+end
---
+++ test/date_helper_test.exs
@@ -0,0 +1,17 @@
+defmodule DateHelperTest do
+  use ExUnit.Case
+  doctest Ibu.DateHelper
+  alias Ibu.DateHelper
+
+  test "to_birth_date" do
+    assert DateHelper.to_birth_date(nil) == nil
+    assert DateHelper.to_birth_date("2020-01-10T12:00:00") == ~D[2020-01-10]
+    assert DateHelper.to_birth_date("2020-01-10T12:00:00Z") == ~D[2020-01-10]
+    assert DateHelper.to_birth_date("2020-01-10") == ~D[2020-01-10]
+  end
+
+  test "to_date_time" do
+    assert DateHelper.to_date_time(nil) == nil
+    assert DateHelper.to_date_time("2020-01-10T12:00:00Z") == DateTime.from_naive!(~N[2020-01-10 12:00:00], "Etc/UTC")
+  end
+end
